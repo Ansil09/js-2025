00e29711a14ce86a6629089bdea93741bfff0b0b
Ansil Shah <40911569+Ansil09@users.noreply.github.com>
2025-06-08 01:25:18 +0000

    variables

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12233;
+let accountEmail = 'ansil@gmail.com';
+var accountPassword = "12345";
+accountCity = "Nepal";
+let accountState;
+
+//accountId = 111111;
+
+console.log(accountId);
+
+//accountId = 121212;
+accountEmail = "shah@gmail.com";
+accountPassword= "23232";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
